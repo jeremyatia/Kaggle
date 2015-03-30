f109ced4b241c9d3da27e32238f50f0c593ecc01
Jeremy ATIA <jeremy@atia.me>
2015-03-30 23:39:33 +0200

    Update 01_data_preprocessing.R
    few changes : indentation, comments

--- 01_data_preprocessing.R
+++ 01_data_preprocessing.R
@@ -1,8 +1,8 @@
 #####################################################################
-# Authors : Stephane FENIAR & Jeremy ATIA - Team "Fast & Curious"	#						
-# Date 	  : March 2015												#		
-# Title   : Kaggle Axa Drivers Challenge							#		
-# Info 	  : Data preprocessing 										#		
+# Authors : Stephane FENIAR & Jeremy ATIA - Team "Fast & Curious"   #						
+# Date 	  : March 2015					 	    #		
+# Title   : Kaggle Axa Drivers Challenge		            #		
+# Info 	  : Data preprocessing 					    #		
 #####################################################################
 
 # Global options 
@@ -21,16 +21,15 @@ NbTrips <- 200
 Summary_Driver_Trips <- function(driver) {
   
 	driver_files <- list.files(path = paste("./drivers/",driver,sep="")) # recover the names of the csv files
-  summary <- matrix(nrow=200, ncol=29) # initialize the matrix which will store the summary of the 200 trips
-  t=1 # trip counter (from 1 to 200)
+  	summary <- matrix(nrow=200, ncol=29) # initialize the matrix which will store the summary of the 200 trips
+  	t=1 # trip counter (from 1 to 200)
   
 	for (file in driver_files){ # for each csv file in each driver folder
-    
-	  #Recover trip data
+    		#Recover trip data
 		coord <- as.matrix(read.csv(paste("./drivers/",driver,"/", file, sep=""))) # x & y coordinates
 		trip  <- as.numeric(strsplit(file,"[.]")[[1]][1])
     
-    #Create some variables for each record of the trip
+    		#Create some variables for each record of the trip
 		time = 1:nrow(coord)
 		dist_to_origin = sqrt(coord[,1]^2 + coord[,2]^2)
 		speed = 0 ; speed[2:nrow(coord)] = sqrt(diff(coord[,1],1)^2 + diff(coord[,2],1)^2)
@@ -42,26 +41,34 @@ Summary_Driver_Trips <- function(driver) {
     
 		#Speed      
 		avg_speed=mean(speed); sd_speed=sd(speed); min_speed=min(speed); max_speed=max(speed)
-		q1_speed=quantile(speed, probs = 0.25); q2_speed=quantile(speed, probs = 0.50); q3_speed=quantile(speed, probs = 0.75)
+		q1_speed=quantile(speed, probs = 0.25)
+		q2_speed=quantile(speed, probs = 0.50)
+		q3_speed=quantile(speed, probs = 0.75)
 		#Acceleration
 		avg_acc=mean(acc); sd_acc=sd(acc); min_acc=min(acc); max_acc=max(acc)
-		q1_acc=quantile(acc, probs = 0.25); q2_acc=quantile(acc, probs = 0.50); q3_acc=quantile(acc, probs = 0.75)
+		q1_acc=quantile(acc, probs = 0.25)
+		q2_acc=quantile(acc, probs = 0.50)
+		q3_acc=quantile(acc, probs = 0.75)
 		#Distance to origin
 		avg_dist_to_orig=mean(dist_to_origin); sd_dist_to_orig=sd(dist_to_origin); max_dist_to_orig=max(dist_to_origin)
-		q1_dist_to_orig=quantile(dist_to_origin, probs = 0.25); q2_dist_to_orig=quantile(dist_to_origin, probs = 0.50); q3_dist_to_orig=quantile(dist_to_origin, probs = 0.75)
+		q1_dist_to_orig=quantile(dist_to_origin, probs = 0.25)
+		q2_dist_to_orig=quantile(dist_to_origin, probs = 0.50)
+		q3_dist_to_orig=quantile(dist_to_origin, probs = 0.75)
 		#Others
-		trip_duration=max(time);  crow_flight=sqrt(coord[nrow(coord), 1]^2 + coord[nrow(coord), 2]^2)
-		acc_time=sum(acc_flag); dec_time=sum(dec_flag)
+		trip_duration=max(time)
+		crow_flight=sqrt(coord[nrow(coord), 1]^2 + coord[nrow(coord), 2]^2)
+		acc_time=sum(acc_flag)
+		dec_time=sum(dec_flag)
 		trip_length = trip_duration * avg_speed
 		crow_ratio = crow_flight / trip_length
 		is_internal = 1 # for now we assume all the trips have been done by this driver
     
 		summary[t,]= cbind(driver, trip, 
-                    avg_speed, sd_speed, min_speed, max_speed, q1_speed, q2_speed, q3_speed, #speed features
+                    		avg_speed, sd_speed, min_speed, max_speed, q1_speed, q2_speed, q3_speed, #speed features
 		                avg_acc, sd_acc, min_acc, max_acc, q1_acc, q2_acc, q3_acc, #acc features
 		                avg_dist_to_orig, sd_dist_to_orig, max_dist_to_orig, q1_dist_to_orig, q2_dist_to_orig, q3_dist_to_orig, #distance to origin features
 		                acc_time, dec_time, trip_duration, crow_flight, trip_length, crow_ratio, is_internal) #others features
-    t=t+1 # increment the trip counter
+    				t=t+1 # increment the trip counter
 	}
 	return(summary) 
 }
@@ -71,17 +78,16 @@ Summary_Driver_Trips <- function(driver) {
 #---------------------------------------------------------------------------------------------------------------#
 DriversTripsSummary = matrix(nrow=NbDrivers * NbTrips, ncol=29)
 colnames(GlobalTripsSummary) =  c('driver', 'trip', 'avg_speed', 'sd_speed', 'min_speed', 'max_speed', 'q1_speed', 'q2_speed', 'q3_speed',
-                       'avg_acc', 'sd_acc', 'min_acc', 'max_acc', 'q1_acc', 'q2_acc', 'q3_acc',
-                       'avg_dist_to_orig', 'sd_dist_to_orig', 'max_dist_to_orig', 'q1_dist_to_orig', 'q2_dist_to_orig', 'q3_dist_to_orig',
-                       'acc_time', 'dec_time', 'trip_duration', 'crow_flight', 'trip_length', 'crow_ratio', 'is_internal')
+                       		'avg_acc', 'sd_acc', 'min_acc', 'max_acc', 'q1_acc', 'q2_acc', 'q3_acc',
+                       		'avg_dist_to_orig', 'sd_dist_to_orig', 'max_dist_to_orig', 'q1_dist_to_orig', 'q2_dist_to_orig', 'q3_dist_to_orig',
+                       		'acc_time', 'dec_time', 'trip_duration', 'crow_flight', 'trip_length', 'crow_ratio', 'is_internal')
 
 d=1 # driver counter
 
 for(driver in Drivers){ 
-  print(d)
-  driverTripsSummary= summary_driver_trips(driver)
-  GlobalTripsSummary[seq((d-1)*NbTrips+1, d*NbTrips),] = driverTripsSummary
-  d=d+1 
+	driverTripsSummary= summary_driver_trips(driver)
+	GlobalTripsSummary[seq((d-1)*NbTrips+1, d*NbTrips),] = driverTripsSummary
+	d=d+1 
 }
 
 # Save the result in a csv file 
